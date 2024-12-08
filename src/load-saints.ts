f712bf85de12b6d2e9c1cb8762128968a199fddd
fang <git@fang.io>
2024-12-08 20:58:17 +0100

    build: revert to loading assets from /assets
    This is a partial revert of 4fdee83. We shouldn't need to move the
    assets folder into dist per se. As long as we point to it correctly
    (still with `__dirname` from the prior commit) it should load fine, as
    the assets folder at the root level is already included in the files for
    the package as specified in `package.json`.

--- src/load-saints.ts
+++ src/load-saints.ts
@@ -32,10 +32,10 @@ type RawSaints = Record<
 
 export function loadSaints(lang: string): Saints {
   const calendar: RawCalendar = parse(
-    readFileSync(__dirname + "/assets/calendar.yml", "utf-8")
+    readFileSync(__dirname + "/../assets/calendar.yml", "utf-8")
   );
   const latinSaints: LatinSaints = parse(
-    readFileSync(__dirname + "/assets/saints/la_VA.yml", "utf-8")
+    readFileSync(__dirname + "/../assets/saints/la_VA.yml", "utf-8")
   );
 
   const saints: Saints = {};
