9408b59d6300f5e478bd7ef9608c0c61ae8fb156
ntvangoor <ntvangoor@gmail.com>
2024-12-09 22:10:05 +0100

    Actually use translation files

--- src/load-saints.ts
+++ src/load-saints.ts
@@ -1,4 +1,3 @@
-import { AssertionError } from "assert";
 import { readFileSync } from "fs";
 import { parse } from "yaml";
 
@@ -14,6 +13,13 @@ type LatinSaints = Record<
   {
     titles?: string[];
     type?: "vigilia" | "domini" | "custom";
+  }
+>;
+
+type LangSaints = Record<
+  string,
+  {
+    name?: string;
   }
 >;
 
@@ -56,26 +62,9 @@ function assertObject(
   });
 }
 
-function parseCalendar(): Calendar {
-  const calendar = parse(
-    readFileSync(__dirname + "/../assets/calendar.yml", "utf-8")
-  );
-
-  assertObject(calendar, ([_class, entry]) => {
-    assert(/[1-4]/.test(_class), `invalid class: ${_class}`);
-    assertObject(entry, ([date, saints]) => {
-      assert(/\d{2}-\d{2}/.test(date), `Invalid date: ${date}`);
-      assertMaybeArray(saints, assertString);
-    });
-  });
-  assert(Object.keys(calendar).length === 4, "calendar needs the keys 1-4");
-
-  return calendar as Calendar;
-}
-
 function parseLatinSaints(): LatinSaints {
   const latinSaints = parse(
-    readFileSync(__dirname + "/../assets/saints/la_VA.yml", "utf-8")
+    readFileSync(`${__dirname}/../assets/saints/la_VA.yml`, "utf-8")
   );
 
   assertObject(latinSaints, ([, saint]) => {
@@ -88,7 +77,7 @@ function parseLatinSaints(): LatinSaints {
           assertMaybeArray(val, assertString);
           break;
         default:
-          throw new Error(`unkonwn key: ${key}`);
+          throw new Error(`unknown key: ${key}`);
       }
     });
   });
@@ -96,46 +85,104 @@ function parseLatinSaints(): LatinSaints {
   return latinSaints as LatinSaints;
 }
 
+function parseCalendar(latinSaints: LatinSaints): Calendar {
+  const calendar = parse(
+    readFileSync(`${__dirname}/../assets/calendar.yml`, "utf-8")
+  );
+
+  assertObject(calendar, ([_class, entry]) => {
+    assert(/[1-4]/.test(_class), `invalid class: ${_class}`);
+    assertObject(entry, ([date, saints]) => {
+      assert(/\d{2}-\d{2}/.test(date), `Invalid date: ${date}`);
+      assertMaybeArray(saints, (val) => {
+        assert(val in latinSaints, `${val} must be in latinSaints`);
+        assertString(val);
+      });
+    });
+  });
+  assert(Object.keys(calendar).length === 4, "calendar needs the keys 1-4");
+
+  return calendar as Calendar;
+}
+
+function parseLangSaints(lang: string, latinSaints: LatinSaints) {
+  const langSaints = parse(
+    readFileSync(`${__dirname}/../assets/saints/${lang}.yml`, "utf-8")
+  );
+
+  assertObject(langSaints, ([key, val]) => {
+    assert(key in latinSaints, `langSaints key: ${key} must be in latinSaints`);
+    assertObject(val, ([key, val]) => {
+      assert(/name/.test(key), `Invalid key ${key}`);
+      assertString(val);
+    });
+  });
+  Object.keys(latinSaints).forEach((key) =>
+    assert(key in langSaints, `latinSaints key: ${key} must be in langSaints`)
+  );
+
+  return langSaints as LangSaints;
+}
+
+function loadSaint(
+  name: string,
+  titles: string | string[],
+  type: LatinSaints[string]["type"],
+  class_: LiturgicalClass
+): Saint {
+  return {
+    title: name.replace("H. ", "H.\u00A0"),
+    subtitle:
+      typeof titles === "string"
+        ? titles
+        : Array.isArray(titles)
+        ? titles.length > 1
+          ? `${titles.slice(0, -1).join(", ")} en ${titles.slice(-1)}`
+          : titles.length
+          ? titles[0]
+          : undefined
+        : undefined,
+    type: type ?? "festum",
+    class: class_,
+  };
+}
+
 export function loadSaints(lang: string): Saints {
-  const calendar = parseCalendar();
   const latinSaints = parseLatinSaints();
-  const langSaints: Saints | null =
-    lang === "la_VA"
-      ? null
-      : parse(readFileSync(`./assets/saints/${lang}.yml`, "utf-8"));
-
+  const calendar = parseCalendar(latinSaints);
   const saints: Saints = {};
-  for (let class_ = 1; class_ <= 4; class_++) {
-    Object.entries(calendar[class_ as LiturgicalClass]).forEach(
-      ([key, val]) => {
-        const [day, month] = key.split("-").map((day) => Number.parseInt(day));
-        const ids = Array.isArray(val) ? val : [val];
-        for (let id of ids) {
-          const saint = latinSaints[id];
-          if (!saint) console.error(`Saint not found: ${id}`);
 
-          saints[month] ??= {};
-          saints[month][day] ??= [];
-          saints[month][day].push({
-            title: langSaints ? langSaints[id].name : id,
-            subtitle:
-              typeof saint.titles === "string"
-                ? saint.titles
-                : Array.isArray(saint.titles)
-                ? saint.titles.length > 1
-                  ? `${saint.titles
-                      .slice(0, -1)
-                      .join(", ")} en ${saint.titles.slice(-1)}`
-                  : saint.titles.length
-                  ? saint.titles[0]
-                  : undefined
-                : undefined,
-            type: saint.type ?? "festum",
-            class: class_ as LiturgicalClass,
-          });
-        }
+  for (
+    let class_: LiturgicalClass = 1;
+    class_ <= 4;
+    class_ = (class_ + 1) as LiturgicalClass
+  ) {
+    Object.entries(calendar[class_]).forEach(([date, val]) => {
+      const [day, month] = date.split("-").map((date) => Number.parseInt(date));
+      const names = Array.isArray(val) ? val : [val];
+
+      for (let name of names) {
+        const latinSaint = latinSaints[name];
+        if (!latinSaint) console.error(`Saint not found: ${name}`);
+
+        saints[month] ??= {};
+        saints[month][day] ??= [];
+        saints[month][day].push(
+          loadSaint(name, latinSaint.titles, latinSaint.type, class_)
+        );
       }
-    );
+    });
+  }
+
+  if (lang !== "la_VA") {
+    const langSaints = parseLangSaints(lang, latinSaints);
+    Object.values(saints).forEach((month) => {
+      Object.values(month).forEach((day) => {
+        day.forEach((saint) => {
+          saint.title = langSaints[saint.title].name;
+        });
+      });
+    });
   }
 
   return saints;
